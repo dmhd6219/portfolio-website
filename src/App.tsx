9d8f20e466cd2ebdf24a0d7142ed29155a7b2518
root <root@dmhd6219-laptop>
2024-09-23 17:59:50 +0300

    init: add lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,8 @@ function App() {
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
-
-
-
-
                 </a>
+
                 <a href="https://react.dev" target="_blank">
                     <img
                         src={reactLogo}
